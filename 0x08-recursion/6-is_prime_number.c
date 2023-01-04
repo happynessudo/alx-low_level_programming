f0c4c13ef9d9a9a74c12ca5b8fd040e80f15e776
happynessudo <happinessudo66@gmail.com>
2023-01-04 12:10:55 -0800

    6th task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -21,7 +21,7 @@ int is_prime(unsigned int n, unsigned int c)
  * is_prime_number - detects if an input number is a prime number.
  * @n: input number.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
- */
+*/
 int is_prime_number(int n)
 {
 	if (n == 0)
@@ -29,6 +29,6 @@ int is_prime_number(int n)
 	if (n < 0)
 		return (0);
 	if (n == 1)
-			return (0);
-			return (is_prime(n, 2));
+		return (0);
+return (is_prime(n, 2));
 }
